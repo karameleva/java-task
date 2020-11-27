70d49b0c0e03d6b0b3732e2cfea9a2dc61835335
Katerina Karameleva <katerina.karameleva@gmail.com>
2020-11-27 23:06:26 +0300

    removed all static methods from NewYearPresent

--- src/main/java/karameleva/java/сollections/maintask/NewYearPresent.java
+++ src/main/java/karameleva/java/сollections/maintask/NewYearPresent.java
@@ -1,17 +1,23 @@
 package karameleva.java.сollections.maintask;
 
 import java.util.*;
-
 import static karameleva.java.сollections.maintask.SweetsType.*;
 
 public class NewYearPresent {
     private static List<? extends Confectionery> confectioneries;
-
+    private Confectionery confectionary;
+    private int amount;
+    public NewYearPresent(Confectionery confectionary, int amount) {
+        this.confectionary = confectionary;
+        this.amount = amount;
+    }
     public NewYearPresent(List<? extends Confectionery> confectioneries) {
         NewYearPresent.confectioneries = confectioneries;
     }
 
-    public static int getAmount(String typeOfConfectionery) {
+    List <NewYearPresent> present = new ArrayList<>();
+
+    public int getAmount(String typeOfConfectionery) {
         Scanner scan = new Scanner(System.in);
         System.out.println(typeOfConfectionery + ".\nВведите количество, которое вы хотите положить в новогодний подарок (шт):  ");
         int amount = scan.nextInt();
@@ -25,8 +31,7 @@ public class NewYearPresent {
     public Map <SweetsType, Integer> getSweetsMap () {
         int amountChocolateSweetsInPresent = getAmount("Шоколадные конфеты");
         int amountCaramelSweetsInPresent = getAmount("Карамель");
-
-       Map<SweetsType, Integer> mapSweets = new HashMap<>();
+        Map <SweetsType, Integer> mapSweets = new HashMap<>();
         mapSweets.put (CHOCOLATE, amountChocolateSweetsInPresent);
         mapSweets.put (CARAMEL, amountCaramelSweetsInPresent);
         return mapSweets;
@@ -35,75 +40,47 @@ public class NewYearPresent {
     public Map <FlourConfectionType, Integer> getFlourConfectionMap () {
         int amountBiscuitsInPresent = getAmount("Печенье");
         int amountGingerbreadInPresent = getAmount("Пряники");
-
         Map <FlourConfectionType, Integer> mapFlourConfection = new HashMap<>();
         mapFlourConfection.put (FlourConfectionType.BISCUITS, amountBiscuitsInPresent);
         mapFlourConfection.put (FlourConfectionType.GINGERBREAD, amountGingerbreadInPresent);
-
         return mapFlourConfection;
     }
 
-    public List <Confectionery>  getNewYearPresent (Map <SweetsType, Integer> mapSweets,
-                                                    Map<FlourConfectionType, Integer> mapFlourConfection) {
-        double presentWeight = 0;
-        List<Confectionery> newYearPresent = new ArrayList<>();
+    public List <NewYearPresent> getMyNewYearPresent () {
+        Map <SweetsType, Integer> mapSweets = getSweetsMap ();
+        Map <FlourConfectionType, Integer> mapFlourConfection = getFlourConfectionMap ();
         for (Confectionery confectionary : confectioneries) {
             if (confectionary instanceof Sweets) {
                 if (mapSweets.get(((Sweets) confectionary).getType()) > 0) {
-                    newYearPresent.add(confectionary);
-                    presentWeight += (confectionary.weight * mapSweets.get(((Sweets) confectionary).getType()));
+                    present.add(new NewYearPresent(confectionary,
+                            mapSweets.get(((Sweets) confectionary).getType())));
                 }
-            } else {
-                if (confectionary instanceof FlourConfection) {
-                    if (mapFlourConfection.get(((FlourConfection) confectionary).getType()) > 0) {
-                        newYearPresent.add(confectionary);
-                        presentWeight += (confectionary.weight * mapFlourConfection.get(((FlourConfection) confectionary).getType()));
-                    }
+            } else if (confectionary instanceof FlourConfection) {
+                if (mapFlourConfection.get(((FlourConfection) confectionary).getType()) > 0) {
+                    present.add(new NewYearPresent(confectionary,
+                            mapFlourConfection.get(((FlourConfection) confectionary).getType())));
                 }
             }
         }
-        if (newYearPresent.isEmpty()) {
+        if (present.isEmpty()) {
             throw new IllegalArgumentException("Новогодний подарок не собран!");
         }
-            return newYearPresent;
+        return present;
     }
 
-    public void printNewYearPresent (List <Confectionery> newYearPresent,
-                                            Map <SweetsType, Integer> mapSweets,
-                                            Map<FlourConfectionType, Integer> mapFlourConfection) {
-        System.out.println("\nНовогодний подарок: ");
-        for (Confectionery confectionary : newYearPresent) {
-            if (confectionary instanceof Sweets) {
-                System.out.println("'" + confectionary.getName() + "' (" +
-                        ((Sweets) confectionary).getType().getRealName() + "), " +
-                        mapSweets.get(((Sweets) confectionary).getType()) + " шт.");
-            } else if (confectionary instanceof FlourConfection) {
-                System.out.println("'" + confectionary.getName() + "' (" +
-                        ((FlourConfection) confectionary).getType().getRealName() + "), " +
-                        mapFlourConfection.get(((FlourConfection) confectionary).getType()) + " шт.");
-            }
-        }
-    }
-
-    public static double getNewYearPresentWeight (List <Confectionery> newYearPresent,
-                                           Map <SweetsType, Integer> mapSweets,
-                                           Map<FlourConfectionType, Integer> mapFlourConfection) {
+    public double getNewYearPresentWeight() {
         double presentWeight = 0;
-        for (Confectionery confectionary : newYearPresent) {
-            if (confectionary instanceof Sweets) {
-                presentWeight += (confectionary.weight * mapSweets.get(((Sweets) confectionary).getType()));
-            } else if (confectionary instanceof FlourConfection) {
-                presentWeight += (confectionary.weight * mapFlourConfection.get(((FlourConfection) confectionary).getType()));
-            }
+        for (NewYearPresent presentElement : present) {
+            presentWeight += presentElement.confectionary.weight * presentElement.amount;
         }
         return presentWeight;
     }
 
-    public List <Sweets> getSweetsListInNewYearPresent(List <Confectionery> newYearPresent) {
+    public List <Sweets> getSweetsListInNewYearPresentSortedByWeight() {
         List <Sweets> sweets = new ArrayList();
-        for (Confectionery confectionery : newYearPresent) {
-            if (confectionery instanceof Sweets) {
-                sweets.add((Sweets) confectionery);
+        for (NewYearPresent presentElement : present) {
+            if (presentElement.confectionary instanceof Sweets) {
+                sweets.add((Sweets) presentElement.confectionary);
             }
         }
         if (sweets.isEmpty()) {
@@ -113,7 +90,7 @@ public class NewYearPresent {
         return sweets;
     }
 
-    public List <Sweets> searchSweetAccordingToSugar(List <Sweets> sweets) {
+    public List <Sweets> searchSweetAccordingToSugar() {
         Scanner scan = new Scanner(System.in);
         System.out.println("\nДля поиска конфеты по содержанию сахара, введите минимальное количество сахара в конфете");
         int minSugar = scan.nextInt();
@@ -124,24 +101,37 @@ public class NewYearPresent {
             minSugar = scan.nextInt();
         }
         System.out.println("Введите максимальное количество сахара в конфете");
-
         int maxSugar = scan.nextInt();
         while (maxSugar < 0) {
             System.out.println("Введено некорректное количество. Попробуйте еще раз. " +
                     "\nВведите максимальное количество сахара в конфете:  ");
             maxSugar = scan.nextInt();
         }
-
-        List <Sweets> sweetsAccordingToSugar = new ArrayList<>();
-        for (Sweets sweet : sweets) {
-            if (sweet.getSugarValue() > minSugar &&
-                    sweet.getSugarValue() < maxSugar) {
-                sweetsAccordingToSugar.add(sweet);
+        List <Sweets> sweets  = getSweetsListInNewYearPresentSortedByWeight();
+        for (int i = 0; i < sweets.size(); i++) {
+            if (sweets.get(i).getSugarValue() < minSugar || sweets.get(i).getSugarValue() > maxSugar) {
+                sweets.remove(i);
+                i--;
             }
         }
-        if (sweetsAccordingToSugar.isEmpty()) {
+        if (sweets.isEmpty()) {
             throw new IllegalArgumentException("Конфеты с заданым содержанием сахара не найдены!");
         }
-        return sweetsAccordingToSugar;
+        return sweets;
+    }
+
+    @Override
+    public String toString() {
+        String presentToPrint = null;
+        if (confectionary instanceof Sweets) {
+            presentToPrint =  "\n'" + confectionary.name + "' (" +
+                    ((Sweets) confectionary).getType() +
+                    "), " + amount + "шт.";
+        } else if (confectionary instanceof FlourConfection) {
+            presentToPrint = "\n'" + confectionary.name + "' (" +
+                    ((FlourConfection) confectionary).getType() +
+                    "), " + amount + "шт.";
+        }
+        return presentToPrint;
     }
 }
